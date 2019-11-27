2a85f6492ab69f27c4369e3fdafcf674a025082f
BOSUNG <endoo76767@sasa.hs.kr>
2019-11-27 10:20:59 +0900

    score adding

--- ending_game.py
+++ ending_game.py
@@ -1,6 +1,8 @@
+#사용 방식 : ending_game 함수 호출 (display, score) 변수 사용
+
 import pygame, time
 
-black = (0, 0, 0)
+vanilla = (255, 204, 154)
 
 def ending_game(display, score):
     clock = pygame.time.Clock()
@@ -8,6 +10,7 @@ def ending_game(display, score):
     x = 300
     sum = 5
     pivot1 = 1
+
     while pivot:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,66 +21,72 @@ def ending_game(display, score):
                     return True
                 else:
                     return False
-        display.fill(black)
+        display.fill(vanilla)
 
         if x > 600:
             pivot1 = 1
             sum = -1
         elif x > 580:
             if pivot1 == 1:
-                sum = -2
+                sum = -1.5
             else:
-                sum = 2
+                sum = 1.5
         elif x > 500:
             if pivot1 == 1:
-                sum = -3
+                sum = -2.5
             else:
-                sum = 3
+                sum = 2.5
         elif x > 400:
             if pivot1 == 1:
-                sum = -5
+                sum = -4
             else:
-                sum = 5
+                sum = 4
         elif x > 300:
             if pivot1 == 1:
-                sum = -6
-            else:
-                sum = 6
-        elif x > 200:
-            if pivot1 == 1:
                 sum = -5
             else:
                 sum = 5
+        elif x > 200:
+            if pivot1 == 1:
+                sum = -4
+            else:
+                sum = 4
         elif x > 100:
             if pivot1 == 1:
-                sum = -3
+                sum = -2.5
             else:
-                sum = 3
+                sum = 2.5
         elif x > 20:
             if pivot1 == 1:
-                sum = -2
+                sum = -1.5
             else:
-                sum = 2
+                sum = 1.5
         elif x < 0:
             pivot1 = 0
             sum = 1
         x += sum
 
-        pygame.draw.rect(display, (255, 255, 255), [x, 0, 100, 100], 3)
-        pygame.draw.rect(display, (255, 255, 255), [600 - x, 600, 100, 100], 3)
-        pygame.draw.rect(display, (255, 255, 255), [0, 600 - x, 100, 100], 3)
-        pygame.draw.rect(display, (255, 255, 255), [600, x, 100, 100], 3)
+        pygame.draw.rect(display, (255, 204, 204), [x, 0, 100, 100])
+        pygame.draw.rect(display, (255, 255, 255), [x, 0, 100, 100], 6)
+        pygame.draw.rect(display, (255, 204, 204), [600 - x, 600, 100, 100])
+        pygame.draw.rect(display, (255, 255, 255), [600 - x, 600, 100, 100], 6)
+        pygame.draw.rect(display, (255, 204, 204), [0, 600 - x, 100, 100])
+        pygame.draw.rect(display, (255, 255, 255), [0, 600 - x, 100, 100], 6)
+        pygame.draw.rect(display, (255, 204, 204), [600, x, 100, 100])
+        pygame.draw.rect(display, (255, 255, 255), [600, x, 100, 100], 6)
         scorefont = pygame.font.SysFont("comicsansms", 50)
-        label1 = scorefont.render("          Game Over!!", 1, (255, 255, 255))
-        label2 = scorefont.render("          score : %d" % score, 1, (255, 255, 255))
-        label3 = scorefont.render("   press down to restart", 1, (255, 255, 255))
+        scorefont2 = pygame.font.SysFont("comicsansms", 50)
+        label1 = scorefont2.render("Game Over!!", 1, (102, 51, 51))
+        label2 = scorefont2.render("score : %d" % score, 1, (102, 51, 51))
+        label3 = scorefont2.render("press      to restart", 1, (102, 51, 51))
+        pygame.draw.polygon(display, (255, 255, 255), [[285, 420], [335, 420], [310, 460]])
         label4 = scorefont.render(" A+", 1, (255, 255, 255))
         label5 = scorefont.render(" C-", 1, (255, 255, 255))
-        label6 = scorefont.render(" F", 1, (255, 255, 255))
+        label6 = scorefont.render(" D", 1, (255, 255, 255))
         label7 = scorefont.render(" B0", 1, (255, 255, 255))
-        display.blit(label1, (50, 200))
-        display.blit(label2, (50, 300))
-        display.blit(label3, (50, 400))
+        display.blit(label1, (230, 200))
+        display.blit(label2, (235, 300))
+        display.blit(label3, (140, 400))
         display.blit(label4, (x, 10))
         display.blit(label5, (610 - x, 610))
         display.blit(label6, (20, 610 - x))
--- score_sum.py
+++ score_sum.py
@@ -1,1 +1,3 @@
 #점수 합산 구현
+
+def score_sum(total_sum, add):
