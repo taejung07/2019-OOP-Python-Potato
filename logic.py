e83946bba639ce4c03e19d5d7b97556ca30d6178
user <syd5462>
2019-11-30 20:14:29 +0900

    update

---
+++ logic.py
@@ -0,0 +1,132 @@
+import random
+import pygame
+
+background = [[-1]*4 for i in range(4)]
+
+class Block:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move(self):
+        screen.blit(self.image, (self.x, self.y))
+
+class D_plus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("D+.PNG")
+
+class C_plus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("C+.PNG")
+
+class C_zero(Block):
+    def __init__(self):
+        self.image = pygame.image.load("C0.PNG")
+
+class C_minus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("C-.PNG")
+
+class B_plus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("B+.PNG")
+
+class B_zero(Block):
+    def __init__(self):
+        self.image = pygame.image.load("B0.PNG")
+
+class B_minus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("B-.PNG")
+
+class A_plus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("A+.PNG")
+
+class A_zero(Block):
+    def __init__(self):
+        self.image = pygame.image.load("A0.PNG")
+
+class A_minus(Block):
+    def __init__(self):
+        self.image = pygame.image.load("A-.PNG")
+
+def new_block(): #두 번씩 실행해야 함.
+    random_x1 = random.randint(0, 3)
+    random_y1 = random.randint(0, 3)
+    if background[random_x1][random_y1] == -1:
+        background[random_x1][random_y1] = 0
+    else:
+        new_block()
+
+
+def combine_block():
+    key = pygame.key.get_pressed()
+    blank = []
+    if key[pygame.K_UP]:
+        for i in range(0, 4):
+            for j in range(3, -1, -1):
+                for k in range(j-1, -1, -1):
+                    if background[j][i] == background[k][i]:
+                        background[k][i] +=1
+                        background[j][i] = -1
+        for i in range(0, 4):
+            for j in range(0, 4):
+                if background[j][i] != -1:
+                    blank.append(j)
+            for k in range(len(blank)):
+                background[k][i] = background[blank[k]][i]
+            for k in range(len(blank)+1, 4):
+                background[k][i] = -1
+
+    elif key[pygame.K_DOWN]:
+        flag = 0
+        for i in range(0, 4):
+            for j in range(0, 4):
+                for k in range(j+1, 4):
+                    if background[j][i] == background[k][i]:
+                        background[k][i] += 1
+                        background[j][i] = -1
+        for i in range(0, 4):
+            for j in range(3, -1, -1):
+                if background[j][i] != -1:
+                    blank.append(j)
+            for k in range(3, 3-len(blank), -1):
+                background[k][i] = background[blank[flag]][i]
+                flag += 1
+            for k in range(3-len(blank), -1, -1):
+                background[k][i] = -1
+
+    elif key[pygame.K_RIGHT]:
+        flag = 0
+        for i in range(0, 4):
+            for j in range(0, 4):
+                for k in range(j+1, 4):
+                    if background[i][j] == background[i][k]:
+                        background[i][k] += 1
+                        background[i][j] = -1
+        for i in range(0, 4):
+            for j in range(3, -1, -1):
+                if background[i][j] != -1:
+                    blank.append(j)
+            for k in range(3, 3-len(blank), -1):
+                background[i][k] = background[i][blank[flag]]
+                flag += 1
+            for k in (3-len(blank), -1, -1):
+                background[i][k] = -1
+
+    elif key[pygame.K_LEFT]:
+        for i in range(0, 4):
+            for j in range(3, -1, -1):
+                for k in range(j-1, -1, -1):
+                    if background[i][j] == background[i][k]:
+                        background[i][k] += 1
+                        background[i][j] = -1
+        for i in range(0, 4):
+            for j in range(0, 4):
+                if background[i][j] != -1:
+                    blank.append(j)
+            for k in range(0, len(blank)+1):
+                background[i][k] = background[i][blank[k]]
+            for k in range(len(blank)+1, 4):
+                background[i][k] = -1
--- yedam-test0.py
+++ yedam-test0.py
@@ -1,17 +1,20 @@
 import pygame
 
+pygame.init()
+
 class Bird(object):  # represents the bird, not the game
     def __init__(self):
         """ The constructor of the class """
         self.image = pygame.image.load("D+.PNG")
         # the bird's position
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.x = 0
         self.y = 0
 
     def handle_keys(self):
         """ Handles Keys """
         key = pygame.key.get_pressed()
-        dist = 1 # distance moved in 1 frame, try changing it to 5
+        dist = 50 # distance moved in 1 frame, try changing it to 5
         if key[pygame.K_DOWN]: # down key
             self.y += dist # move down
         elif key[pygame.K_UP]: # up key
@@ -26,12 +29,26 @@ class Bird(object):  # represents the bird, not the game
         # blit yourself at your current position
         surface.blit(self.image, (self.x, self.y))
 
-
-pygame.init()
 screen = pygame.display.set_mode((640, 400))
 
 bird = Bird() # create an instance
 clock = pygame.time.Clock()
+
+def move(bird, start_x, start_y, end_x, end_y, screen):
+    for i in range(start_x, end_x):
+        bird.x = i
+        clock.tick(120)
+        bird.draw(screen)
+        pygame.display.update()
+        pygame.display.flip()
+
+    for j in range(start_y, end_y):
+        bird.y = j
+        clock.tick(120)
+        bird.draw(screen)
+        pygame.display.update()
+        pygame.display.flip()
+
 
 running = True
 while running:
@@ -46,5 +63,7 @@ while running:
     screen.fill((255,255,255)) # fill the screen with white
     bird.draw(screen) # draw the bird to the screen
     pygame.display.update() # update the screen
+
+    move(bird, 0, 0, 100, 100, screen)
 
     clock.tick(40)
